7d9d95c90b019ffeabf84a82c96c75490350c7c0
Júlia <julia.valadares99@gmail.com>
2021-01-31 18:30:14 -0300

    tive que criar outro site pois o outro foi inicializado de forma errada. Adicionei a venv, criei os models e o admin

---
+++ site2.0/eng_software_env/mysite/courses/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Material(models.Model):
+    class Meta:
+        abstract = True
+    
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    especialidade = models.CharField(max_length=100)
+
+class Site(Material):
+    link = models.CharField(max_length=200)
+
+class Livro(Material):
+    disponibilidade = models.BooleanField(default=True)
+    autor = models.CharField(max_length=200)
+    edicao = models.IntegerField()
+
+class Video(Material):
+    link = models.CharField(max_length=200)
+    canal = models.CharField(max_length=200)
+    duracao = models.DurationField()
+
+
+class Professor(Material):
+    sala = models.IntegerField()
+    email = models.EmailField()
+
+class Materia(Material):
+    codigo = models.CharField(max_length=10)
