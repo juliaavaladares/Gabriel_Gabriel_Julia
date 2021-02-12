af8d18b19f3abff888fa1b41ae18e854407fd182
diiorio7 <gabrieliorio1@hotmail.com>
2021-02-12 20:00:27 -0300

    adicionando pre processadores e mudando funcao de comparacao

--- chatterbot/chatbot.py
+++ chatterbot/chatbot.py
@@ -5,13 +5,17 @@ from chatterbot.trainers import ListTrainer
 chatbot = ChatBot(
     'Turing',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace',
+        'chatterbot.preprocessors.convert_to_ascii'
+    ],
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
             #'excluded_words': ['de', 'do', 'da', 'Um', 'Uma', 'Uns'], #palavras que são excluídas pelo algoritmo na hora de procurar para não se confundir
             'default_response': 'Me desculpe mas eu não entendi',
-            'maximum_similarity_threshold': 0.65,
-            'statement_comparison_function': 'chatterbot.comparisons.SentimentComparisons'
+            'maximum_similarity_threshold': 0.80,
+            'statement_comparison_function': 'chatterbot.comparisons.LevenshteinDistance'
 
         }, 
         
