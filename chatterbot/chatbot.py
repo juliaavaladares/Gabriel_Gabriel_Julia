2424a759d566dfab0e37b45246d2535956c0223b
magaliais <gabriel.ashm@hotmail.com>
2021-02-18 17:43:13 -0300

    front chatterbot page satisfacao tmj

--- chatterbot/chatbot.py
+++ chatterbot/chatbot.py
@@ -7,13 +7,13 @@ chatbot = ChatBot(
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     preprocessors=[
         'chatterbot.preprocessors.clean_whitespace',
-        'chatterbot.preprocessors.convert_to_ascii'
+        'chatterbot.preprocessors.convert_to_ascii',
     ],
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
             #'excluded_words': ['de', 'do', 'da', 'Um', 'Uma', 'Uns'], #palavras que são excluídas pelo algoritmo na hora de procurar para não se confundir
-            'default_response': 'Me desculpe mas eu não entendi',
+            'default_response': 'Me desculpe, mas eu não entendi',
             'maximum_similarity_threshold': 0.80,
             'statement_comparison_function': 'chatterbot.comparisons.LevenshteinDistance'
 
@@ -23,9 +23,10 @@ chatbot = ChatBot(
     database_uri='sqlite:///database.sqlite3'
 ) 
 
- # Treinando com o arquivo txt  
+ # Treinando com o arquivo txt
 training_data_quesans = open('mineracao.txt').read().splitlines()
 training_data_quesans = open('eng_software.txt').read().splitlines()
+
 
 
 training_data = training_data_quesans
