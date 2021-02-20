0e27cbb3b2c8ad38eabec4af84311d2ec4bfb5e3
Júlia <julia.valadares99@gmail.com>
2021-02-20 16:17:08 -0300

    adicionando o profile.html nas views e na urls

--- site2.0/eng_software_env/mysite/courses/urls.py
+++ site2.0/eng_software_env/mysite/courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.conf.urls import url
 from.import views
 
+app_name = 'courses'
 urlpatterns = [
-    path('', views.index, name='index')
+    url('login', views.user_login, name='login'),
+    url('profile', views.profile, name='profile'),
 ]
